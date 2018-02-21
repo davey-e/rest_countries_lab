74eeecb2313920148caeb9a0c47e2ec4ad8bfdfd
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-21 15:21:07 +0000

    Refactored to remove global variables and refactored getting the selected country details

--- public/index.js
+++ public/index.js
@@ -1,17 +1,17 @@
-let url;
-let countries;
-let ul;
-
 const app = function(){
     url = "https://restcountries.eu/rest/v2/all";
+
+    // display last selected country
     const countryObject = getCountryFromLocalStorage();
     if(countryObject !== null){
         createCountryDetailList(countryObject);
     }
+
+    // get data from api
     makeRequest(url, requestComplete);
-    displayCountryDetails();
 }
 
+// get data from api
 const makeRequest = function(url, callback){
     const request = new XMLHttpRequest();
     request.open("GET", url);
@@ -19,43 +19,40 @@ const makeRequest = function(url, callback){
     request.send();
 }
 
+// callback used in makeRequest to convert api data into array of country objects
 const requestComplete = function(){
     if(this.status !== 200) return;
     const jsonString = this.responseText; //This is a string
-    countries = JSON.parse(jsonString); //This parses the string into a js object
+    const countries = JSON.parse(jsonString); //This parses the string into a js object
     populateList(countries);
 }
 
 const populateList = function(countries){
     const select = document.getElementById("countries-drop-down");
-    countries.forEach(function(country){
+    countries.forEach(function(country, index){
         const option = document.createElement("option");
         option.innerText = country.name;
+        option.value = index;
         select.appendChild(option);
     });
-}
-
-const displayCountryDetails = function(){
-    ul = document.getElementById("country-details");
-    const select = document.getElementById("countries-drop-down");
-    select.addEventListener('change', handleDropDownChange);
-}
-
-const handleDropDownChange = function(){
+    // when the dropdown is used to select a country name:...
+    const handleDropDownChange = function(){
+    // clear previously selected country from displayed details
+    const ul = document.getElementById("country-details");
     ul.innerHTML = "";
-    const select = document.getElementById("countries-drop-down");
-    const selectedCountryName = select.options[select.selectedIndex].innerText;
-    const countryDetails = countries.reduce(function(accumulator, currentValueInArray){
-        if(selectedCountryName === currentValueInArray.name){
-            accumulator = currentValueInArray;
-            
-        };
-        return accumulator;  // the country object
-    })
 
-    createCountryDetailList(countryDetails);
-    saveCountryToLocalStorage(countryDetails);
+    // grab the dropdown list, find what has been selected, 
+    // retrieve the country object from the array of countries
+    const select = document.getElementById("countries-drop-down");
+    const selectedCountry = countries[this.value]
+
+    createCountryDetailList(selectedCountry);
+    saveCountryToLocalStorage(selectedCountry);
     
+    }
+
+    select.addEventListener('change', handleDropDownChange);
+
 }
 
 const saveCountryToLocalStorage = function (countryDetails) {
@@ -68,8 +65,9 @@ const getCountryFromLocalStorage = function () {
     return countryObject;
 }
 
+
 const createCountryDetailList = function (countryDetails) {
-    ul = document.getElementById("country-details");
+    const ul = document.getElementById("country-details");
     const liName = document.createElement('li');
     liName.innerText = "Country Name: " + countryDetails.name;
     ul.appendChild(liName);
