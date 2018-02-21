89df825861acfd7e324734ca1deb5accd78b8e44
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-21 13:49:13 +0000

    Added code to display selected country details when a country is selected from the drop-down

--- public/index.js
+++ public/index.js
@@ -1,8 +1,11 @@
 let url;
+let countries;
+let ul;
 
 const app = function(){
     url = "https://restcountries.eu/rest/v2/all";
     makeRequest(url, requestComplete);
+    displayCountryDetails();
 }
 
 const makeRequest = function(url, callback){
@@ -15,7 +18,7 @@ const makeRequest = function(url, callback){
 const requestComplete = function(){
     if(this.status !== 200) return;
     const jsonString = this.responseText; //This is a string
-    const countries = JSON.parse(jsonString); //This parses the string into a js object
+    countries = JSON.parse(jsonString); //This parses the string into a js object
     populateList(countries);
 }
 
@@ -26,7 +29,35 @@ const populateList = function(countries){
         option.innerText = country.name;
         select.appendChild(option);
     });
+}
 
+const displayCountryDetails = function(){
+    ul = document.getElementById("country-details");
+    const select = document.getElementById("countries-drop-down");
+    select.addEventListener('change', handleDropDownChange);
+}
+
+const handleDropDownChange = function(){
+    ul.innerHTML = "";
+    const select = document.getElementById("countries-drop-down");
+    const selectedCountryName = select.options[select.selectedIndex].innerText;
+    const countryDetails = countries.reduce(function(accumulator, currentValueInArray){
+        if(selectedCountryName === currentValueInArray.name){
+            accumulator = currentValueInArray;
+            
+        };
+        return accumulator;  // the country object
+    })
+    
+    const liName = document.createElement('li');
+    liName.innerText = "Country Name: " + countryDetails.name;
+    ul.appendChild(liName);
+    const liPopulation = document.createElement('li');
+    liPopulation.innerText = "Population: " + countryDetails.population;
+    ul.appendChild(liPopulation);
+    const liCapital = document.createElement('li');
+    liCapital.innerText = "Capital City: " + countryDetails.capital;
+    ul.appendChild(liCapital);
 }
 
 document.addEventListener('DOMContentLoaded', app);
